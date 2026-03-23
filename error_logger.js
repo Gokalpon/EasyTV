05a82e809bd18c5cba45817acfb3251c3bb3f558
Gokalpon <128904950+Gokalpon@users.noreply.github.com>
2026-03-23 21:19:24 +0300

    Update error_logger.js

--- error_logger.js
+++ error_logger.js
@@ -1,12 +1,3 @@
 window.addEventListener('error', function(e) {
-    if (window.__logError_triggered) return;
-    window.__logError_triggered = true;
-    const err = { 
-        msg: e.message, 
-        file: e.filename, 
-        lineno: e.lineno, 
-        colno: e.colno, 
-        err: e.error ? e.error.stack : '' 
-    };
-    console.error('EASYTV_GLOBAL_ERROR', JSON.stringify(err));
+    console.log('Hata yakalandı:', e.message);
 });
