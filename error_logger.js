ab8201413cdd4063f5a85b68dea9dfda2163a226
Gokalpon <gokalpon@gmail.com>
2026-03-23 20:29:50 +0300

    23.03

---
+++ error_logger.js
@@ -0,0 +1,6 @@
+window.addEventListener('error', function(e) {
+    if (window.__logError_triggered) return;
+    window.__logError_triggered = true;
+    const err = { msg: e.message, file: e.filename, lineno: e.lineno, colno: e.colno, err: e.error ? e.error.stack : '' };
+    console.error('EASYTV_GLOBAL_ERROR', JSON.stringify(err));
+});
